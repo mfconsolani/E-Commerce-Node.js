a0bb858bd927f820288719f45a71df009f36197b
MAURO <mfconsolani@outlook.com>
2021-03-09 23:24:00 +0100

    small refactor

--- src/carritoRoutes.ts
+++ src/carritoRoutes.ts
@@ -2,29 +2,33 @@ import express, { Request, Response } from 'express';
 
 import { instanciaCarrito } from './server'
 
+
+
 export const carritoRoutes = express.Router();
+
+
 
 carritoRoutes.get('/', (req: Request, res: Response) => {
 
     instanciaCarrito.listarProductos(req, res);
 
-})
+});
 
 carritoRoutes.get('/:id', (req: Request, res: Response) => {
     
     instanciaCarrito.listarProductoIndividual(req, res);
 
-})
+});
 
 carritoRoutes.post('/:id_producto', (req: Request, res: Response) => {
   
     instanciaCarrito.agregarProducto(req, res);
 
-})
+});
 
 
 carritoRoutes.delete('/:id', (req: Request, res: Response) => {
     
     instanciaCarrito.eliminarProducto(req, res);
 
-})
+});
